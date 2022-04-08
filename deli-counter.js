e46166819faa61897ec37a02ad513ec6586d4cc6
Habtamu Debele <habtlig2010@gmail.com>
2022-04-08 16:39:09 -0400

    Completed Assessment

--- deli-counter.js
+++ deli-counter.js
@@ -1,13 +1,12 @@
 // 1. Write your functions here
 
-
-// 2. Example Usage
-
+// Name: Habtamu Debele
+console.log("\n")
 const katzDeli = []
 function takeANumber(customerList, name){
     let str = ''
     customerList.push(name)
-    customerList.forEach(function (element, position){
+    customerList.map(function (element, position){
         str = `Welcome, ${element}. You are number ${position + 1} in line.`
     })
     console.log(str)
@@ -15,41 +14,50 @@ function takeANumber(customerList, name){
 takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
-
+console.log("\n")
 //console.log(katzDeli)
 
 function line(customerList){
-    let msg = ''
     if(customerList.length > 0){
-        customerList.forEach(function (element, position){
-            msg = `The line is currently: ${position + 1}. ${element}`
-            console.log(msg)
+        const customerPosition = customerList.map(function (customerList, position){
+           return ` ${position + 1}.${customerList}`
         })
+        console.log(`The line is currently: ${customerPosition}`)
     }
     else{
         console.log("The line is currently empty.")
     }
-    
 }
 line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+console.log("\n")
 
 function nowServing(customerList){
-    for(let i = 0; i < customerList.length; i++){
-        console.log(`Currently serving: ${customerList[i]}`)
-        customerList.shift()
-        return line(customerList)
+    if(customerList.length > 0){
+        for(let i = 0; i < customerList.length; i++){
+            console.log(`Currently serving: ${customerList[i]}`)
+            customerList.shift()
+            return line(customerList)
+        }
+    }
+    else{
+        console.log(`"There is nobody waiting to be served!"`)
     }
     
 }
-
 nowServing(katzDeli) //=> "Currently serving Ada."
-
-line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+console.log("\n")
+// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
 
 takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
-
+console.log("\n")
 line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
+console.log("\n")
 nowServing(katzDeli) //=> "Currently serving Grace."
+console.log("\n")
+nowServing(katzDeli) //=> "Currently serving Kent."
+console.log("\n")
+nowServing(katzDeli) //=> "Currently serving Matz."
+console.log("\n")
+nowServing(katzDeli) //"There is nobody waiting to be served!"
 
-line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+//line(katzDeli) //=> "The line is currently empty."
